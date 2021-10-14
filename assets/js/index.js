6966731d47401682caf17e8496935e21a096e034
Yuki XD <pxnditxyr@gmail.com>
2021-10-14 01:52:30 -0400

    feat[js]: Added basic event listener to the action buttons

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,8 @@
+const addButton = document.querySelector( "#add-button" );
+const removeButton = document.querySelector( "#remove-button" );
+
 console.log( "Initializing Testimonial Grid Section Main | Frontend Mentor" );
+
+
+addButton.addEventListener( "click", () => console.log( "Add button clicked" ), false );
+removeButton.addEventListener( "click", () => console.log( "Remove button clicked" ), false );
